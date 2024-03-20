d2f1965dc82184dbb87bec7c5e5572315bddf70c
Parthik <parthik.shah@cognizant.com>
2024-03-20 16:04:26 +0530

    Changes

--- Frontend/src/app/modules/home/favorite-events/favorite-events.component.ts
+++ Frontend/src/app/modules/home/favorite-events/favorite-events.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IEvent } from 'src/app/models/event.interface';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-favorite-events',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteEventsComponent implements OnInit {
 
-  constructor() { }
+  selectedEvent: IEvent | null = null;
+  favoritEvents: any[] = [];
+  constructor(
+    // call homeService in constructor
+    private homeService: HomeService
+  ) { }
 
   ngOnInit(): void {
+
+    this.getFavoriteEvents();
   }
+
+
+  // create a function to get favorite events
+  getFavoriteEvents() {
+    // return this.homeService.getFavoriteEvents();
+  }
+
+  // creae method to close event details
+
+
+  /**
+ * Closes the event details.
+ */
+  closeEventDetails() {
+    this.selectedEvent = null;
+
+  }
+
+// create method to show details of event
+
+  /**
+   * Shows the details of the selected event.
+   * @param event The selected event.
+   */
+  showEventDetails(event: IEvent) {
+    this.selectedEvent = event;
+  }
+
+  deleteEvent(event: IEvent){
+    
+  }
+
+
+
 
 }
